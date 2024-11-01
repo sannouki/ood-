c6cd6b117bb9e42f5183106e4f22ead7c3f79f07
sannouki <167042459+sannouki@users.noreply.github.com>
2024-11-01 12:55:56 -0400

    Update ManufacturerB.java

--- ood homework problem 2/ManufacturerB.java
+++ ood homework problem 2/ManufacturerB.java
@@ -1,8 +1,10 @@
-public class ManufacturerB implements Manufacturer{
-    //Manufacturer B focuses on creating drones with accurate flightrange for the job
-    
-    //blueprint to make drones surveillance, delivery, and agriculturals
-    //allow manufacturer to customize drones details such as the range the battery and features.
+public class ManufacturerB implements Manufacturer {
+    // Manufacturer B focuses on creating drones with accurate flightrange for the
+    // job
+
+    // blueprint to make drones surveillance, delivery, and agriculturals
+    // allow manufacturer to customize drones details such as the range the battery
+    // and features.
     @Override
     public SurveillanceDrone createSurveillanceDrone() {
         return new SurveillanceDrone(500, 150.50, "Camera with night vision");
@@ -12,18 +14,28 @@ public class ManufacturerB implements Manufacturer{
     public DeliveryDrone createDeliveryDrone() {
         return new DeliveryDrone(500, 125.8, "carrying packages and delivering");
     }
-    
-    //@Override
-    //public AgricultureDrone createAgricultureDrone() {
-    //    return new AgricultureDrone(150, 500, "planting and cropping");
+
+    // @Override
+    // public AgricultureDrone createAgricultureDrone() {
+    // return new AgricultureDrone(150, 500, "planting and cropping");
 
     @Override
     public AgricultureDrone createAgricultureDrone() {
         return new AgricultureDrone(15, 5, "planting and cropping");
     }
 
-    // Overloaded method to create an agriculture drone with custom parameters
+    // Overloaded method change drones custom details
     public AgricultureDrone createAgricultureDrone(int battery, double flightRange, String specialFeature) {
         return new AgricultureDrone(battery, flightRange, specialFeature);
     }
+
+    public DeliveryDrone createDeliveryDrone(int battery, double flightRange, String specialFeature) {
+        return new DeliveryDrone(battery, flightRange, specialFeature);
+    }
+
+    // Overloaded method to create an agriculture drone with custom parameters
+    public SurveillanceDrone createSurveillanceDrone(int battery, double flightRange, String specialFeature) {
+        return new SurveillanceDrone(battery, flightRange, specialFeature);
+    }
 }
+
